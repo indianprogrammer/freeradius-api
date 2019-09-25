e5e8d0ff2f2ab0800852b5896e0441997cfd7c3e
manoj <manoj.singh@radiolocus.com>
2019-09-25 18:57:34 +0530

    user controller added

---
+++ application/controllers/v1/User.php
@@ -0,0 +1,35 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class User extends CI_Controller {
+    use REST_Controller { REST_Controller::__construct as private __resTraitConstruct; }
+
+    function __construct(){
+        // Construct the parent class
+        parent::__construct();
+        $this->__resTraitConstruct();
+    }
+
+    function index_get(){
+        echo "test";
+    }
+
+}
